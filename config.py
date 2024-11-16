af03685a2aad0133fe32037c8d0485e0ad26467f
speed666 <speedonespeed@outlook.sa>
2024-11-16 08:48:31 -0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+from sample_config import Config
+
+
+class Development(Config):
+    # ضـع اكـوادك تحت مكـان الكـلام العـربي  .. امسـح الكلام العربي اللي بين " " فقـط وضع اكـوادك
+  
+    APP_ID = 22665066 
+    API_HASH = "92dbe89d182f72f427972d8993850130"
+    ALIVE_NAME = "اسم_حسابك" 
+    DB_URI = "postgresql://postgres:%vy=xZprdOce3Xf+a#3E@localhost:5432/speed" # قاعده البيانات 
+    STRING_SESSION = "كود_ترمكس"
+    TG_BOT_TOKEN = "توكن_بوتك"
+    PRIVATE_GROUP_BOT_API_ID = -1002456966069   # ضـع ايـدي كـروب السجـل بجـانب العـدد -100 لا تمسـح العـدد هـذا
+    PM_LOGGER_GROUP_ID  = -1002139622985 # ضـع ايـدي كـروب التخزين بجـانب العـدد -100 لا تمسـح العـدد هـذا
+    COMMAND_HAND_LER = "."  # اتركهــا كمـا هـي
+    SUDO_COMMAND_HAND_LER = "."  # اتركهــا كمـا هـي
+    TZ = "Africa/Cairo"  # اتركهــا كمـا هـي - حسب المنطقه
